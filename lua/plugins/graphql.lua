b37a260a7b83ece4843cadd43c9df0920de6ea9e
samiam376 <sam@withshepherd.com>
2023-11-03 13:45:52 -0700

    add treesitter
    Signed-off-by: samiam376 <sam@withshepherd.com>

--- lua/plugins/graphql.lua
+++ lua/plugins/graphql.lua
@@ -1,6 +1,15 @@
 return {
 
   {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "graphql",
+      },
+    },
+  },
+
+  {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
